c3382149e05b6618ce9f2837ec20eb74c35d288d
Franck Espinosa <franck@espinosa.eu>
2021-10-11 11:35:20 +0200

    Add project files.

---
+++ VLCStream/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace VLCStream
+{
+    public partial class MainWindow : Window
+    {
+        public View Page { get; set; }
+
+        public string URL { get; set; } = "https://youtu.be/wTcNtgA6gHs";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Page = new View();
+            vlcFrame.Content = Page;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn.Content.Equals("Play"))
+            {
+                Page.StartPlay(URL);
+                btn.Content = "Stop";
+            }
+            else
+            {
+                Page.StopPlay();
+                btn.Content = "Play";
+            }
+        }
+    }
+}
---
+++ VLCStream/View.xaml.cs
@@ -0,0 +1,65 @@
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace VLCStream
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class View : Page
+    {
+        public LibVLC libVLC { get; set; }
+
+        public View()
+        {     
+            Core.Initialize();   
+            InitializeComponent();
+            libVLC = new LibVLC(MediaOptions()); 
+        }
+
+        public async void StartPlay(string path)
+        {
+            await Dispatcher.InvokeAsync(new Action(async () =>
+            {
+                using (var media = new Media(libVLC, path, FromType.FromLocation))
+                {
+                    await media.Parse(MediaParseOptions.ParseNetwork);
+                    if (media.SubItems.FirstOrDefault() != null)
+                    {
+                        var vlcPlayer = new MediaPlayer(media.SubItems.FirstOrDefault());
+                        VideoView.MediaPlayer = vlcPlayer;
+                        VideoView.MediaPlayer.Play();
+                    }
+                }                  
+            }));
+        }
+
+        public void StopPlay()
+        {
+            if ((VideoView.MediaPlayer != null) && VideoView.MediaPlayer.IsPlaying)
+                VideoView.MediaPlayer.Stop();
+        }
+
+        /// <summary>
+        /// Pass libVLC options, separated by a comma
+        /// </summary>
+        /// <param name="repeat"></param>
+        /// <returns></returns>
+        private string[] MediaOptions()
+        {
+            string[] mediaOptions;
+
+            mediaOptions = new[] { "--input-repeat=5", "--network-caching=10000", "--avcodec-hw=none" };
+            return mediaOptions;
+        }
+
+        private void Page_Unloaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            VideoView.MediaPlayer.Dispose();
+            libVLC.Dispose();
+        }
+    }
+}
